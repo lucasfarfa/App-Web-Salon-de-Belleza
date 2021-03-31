608f5162c2b1eb8bb193158f273269405107f654
Lucas Farfallini <farfallinilucas19@gmail.com>
2021-03-31 12:11:26 -0300

    primera conexion a la db mediante php

---
+++ AppSalon_inicio/includes/database.php
@@ -0,0 +1,9 @@
+<?php
+
+
+$db = mysqli_connect('localhost', 'root', 'root', 'appsalon'); // host, user , password, database
+
+if (!$db) { // corta el programa si se produce algun error
+    echo 'Error trying to connect to database.';
+    exit;
+}  
